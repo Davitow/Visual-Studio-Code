222b6ad66b94dfdfaebabf96a44efd78fd2f317e
Davito <davezz.enyz@gmail.com>
2024-12-22 16:40:07 +0100

    Withered Golden Freddy

--- JS/Name.js
+++ JS/Name.js
@@ -6,8 +6,10 @@ function displayGreeting() {
     if (userName.trim() !== "") {
         //Vis hilsen med det skrevet inn navnet//
         document.getElementById("nameOutput").innerHTML = "Hello, " + userName + "!";
+        document.getElementById("nameOutput").style.color = "black"; //Setter fargen på hilsen teksten til svart//
     } else {
         //Hvis ingen navn er skrevet inn, spør brukeren om å skrive in navn//
-        document.getElementById("nameOutput").innerHTML = "Skriv inn navn";
+        document.getElementById("nameOutput").innerHTML = "Skriv inn navn"; //Viser feilmelding om submit knappen er trykket og ingenting er skrevet in//
+        document.getElementById("nameOutput").style.color = "red"; //Sett fargeteksten til rød på feilmeldingen//
     }
 }
